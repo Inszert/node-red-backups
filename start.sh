fff9eafc32139dcc55362b5ea5f7823d34dfb414
Daniel Szabo <danielszabo.main@gmail.com>
2025-03-17 17:06:58 +0100

    fix

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
-
 #!/bin/sh
-# Clone the latest Node-RED flows from GitHub
-git clone https://github.com/Inszert/node-red-backups.git /tmp/flows
 
-# Move flows to the correct Node-RED directory
-mkdir -p /data
-mv /tmp/flows/flows.json /data/flows.json
-mv /tmp/flows/flows_cred.json /data/flows_cred.json
-
-# Start Node-RED
-npx node-red --userDir /data
+# Start Node-RED with the flows file in the current directory
+npx node-red --settings ./settings.js
