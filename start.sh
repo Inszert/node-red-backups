270d09a140b969b0a0a2fb970f15b4ca33e4bf63
Daniel Szabo <104948180+Inszert@users.noreply.github.com>
2025-03-17 15:08:14 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,12 @@
 
+#!/bin/sh
+# Clone the latest Node-RED flows from GitHub
+git clone https://github.com/Inszert/node-red-backups.git /tmp/flows
+
+# Move flows to the correct Node-RED directory
+mkdir -p /data
+mv /tmp/flows/flows.json /data/flows.json
+mv /tmp/flows/flows_cred.json /data/flows_cred.json
+
+# Start Node-RED
+npx node-red --userDir /data
